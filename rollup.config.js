a38f9ff6574e4c4f92018f94b21cd9b848bf7826
Arnelle Balane <arnellebalane@gmail.com>
2019-10-26 22:51:51 -0700

    Create ESM and UMD builds for main module; Add clean npm script

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,24 @@
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: [
-    'dot-grid.js',
-    'dot-grid-worklet.js'
-  ],
-
-  output: {
-    dir: 'dist',
+export default [{
+  input: 'dot-grid.js',
+  output: [{
+    file: 'dist/dot-grid.esm.js',
     format: 'esm'
-  },
-
+  }, {
+    file: 'dist/dot-grid.umd.js',
+    format: 'umd'
+  }],
   plugins: [
     terser()
   ]
-};
+}, {
+  input: 'dot-grid-worklet',
+  output: {
+    file: 'dist/dot-grid-worklet.js',
+    format: 'esm'
+  },
+  plugins: [
+    terser()
+  ]
+}];
