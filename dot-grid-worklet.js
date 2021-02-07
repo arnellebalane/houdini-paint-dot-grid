305af025ae10c1e14fb48f11288f92e287b1de5e
Arnelle Balane <arnellebalane@gmail.com>
2021-02-07 19:32:16 +0800

    Modify worklet class to not assume that properties are registered

--- dot-grid-worklet.js
+++ dot-grid-worklet.js
@@ -1,36 +1,43 @@
-class DotGridWorket {
+if (typeof registerPaint === 'function') {
+  class DotGridWorket {
+    static get inputProperties() {
+      return ['--dot-size', '--dot-spacing', '--dot-color'];
+    }
 
-  static get inputProperties() {
-    return ['--dot-size', '--dot-spacing', '--dot-color'];
-  }
+    parseProps(props) {
+      const dotSize = props.get('--dot-size');
+      const dotSpacing = props.get('--dot-spacing');
+      const dotColor = props.get('--dot-color');
+      const isUnknown = (value) => value instanceof CSSUnparsedValue;
 
-  static get contextOptions() {
-    return {alpha: true};
-  }
+      return {
+        dotSize: isUnknown(dotSize) ? parseInt(dotSize.toString(), 10) || 3 : dotSize.value,
+        dotSpacing: isUnknown(dotSpacing) ? parseInt(dotSpacing.toString(), 10) || 6 : dotSpacing.value,
+        dotColor: isUnknown(dotColor) ? dotColor.toString().trim() || '#d2d6db' : dotColor.toString(),
+      };
+    }
 
-  paint(context, size, props) {
-    const dotSize = props.get('--dot-size').value;
-    const dotSpacing = props.get('--dot-spacing').value;
-    const dotColor = props.get('--dot-color').toString();
+    paint(context, size, props) {
+      const { dotSize, dotSpacing, dotColor } = this.parseProps(props);
 
-    const radius = dotSize / 2;
-    const rows = Math.ceil((size.height - radius) / (dotSize + dotSpacing));
-    const cols = Math.ceil((size.width - radius) / (dotSize + dotSpacing));
+      const radius = dotSize / 2;
+      const rows = Math.ceil((size.height - radius) / (dotSize + dotSpacing));
+      const cols = Math.ceil((size.width - radius) / (dotSize + dotSpacing));
 
-    context.fillStyle = dotColor;
+      context.fillStyle = dotColor;
 
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < cols; j++) {
-        const x = (j * (dotSize + dotSpacing)) + radius;
-        const y = (i * (dotSize + dotSpacing)) + radius;
+      for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+          const x = j * (dotSize + dotSpacing) + radius;
+          const y = i * (dotSize + dotSpacing) + radius;
 
-        context.beginPath();
-        context.arc(x, y, radius, 0, Math.PI * 2);
-        context.fill();
+          context.beginPath();
+          context.arc(x, y, radius, 0, Math.PI * 2);
+          context.fill();
+        }
       }
     }
   }
 
+  registerPaint('dot-grid', DotGridWorket);
 }
-
-registerPaint('dot-grid', DotGridWorket);
--- dot-grid.js
+++ dot-grid.js
@@ -3,24 +3,20 @@ if (CSS.registerProperty) {
     name: '--dot-size',
     syntax: '<length>',
     inherits: false,
-    initialValue: '3px'
+    initialValue: '3px',
   });
 
   CSS.registerProperty({
     name: '--dot-spacing',
     syntax: '<length>',
     inherits: false,
-    initialValue: '6px'
+    initialValue: '6px',
   });
 
   CSS.registerProperty({
     name: '--dot-color',
     syntax: '<color>',
     inherits: false,
-    initialValue: '#d2d6db'
+    initialValue: '#d2d6db',
   });
-}
-
-if (CSS.paintWorklet) {
-  CSS.paintWorklet.addModule('./dot-grid-worklet.js');
 }
